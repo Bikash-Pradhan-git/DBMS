133a5f850cd34f8a9691a0d600297b4b5d28d3c9
Bikash Pradhan <121172268+Bikash-Pradhan-git@users.noreply.github.com>
2024-08-27 19:13:46 +0530

    Add files via upload

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.DATAACCESS;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class MovieController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            //return View(ll);
+            MovieDAL movieDAL = new MovieDAL();
+
+            movieDAL.show();
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DATAACCESS/MovieDAL.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.DATAACCESS
+{
+    public class MovieDAL
+    {
+        
+        public void show()
+        {
+
+            //System.Diagnostics.Debug.WriteLine(mv.Id);
+            string queryString = "SELECT Id, Title,Description,Genre,Duration,ReleaseDate FROM dbo.Movies;";
+
+            using (SqlConnection connection = new SqlConnection("data source=BFL-COMP-7472\\SQLEXPRESS;database=MovieBooking;Trusted_Connection=True"))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // get the results of each column
+                    int id = (int)reader["Id"];
+
+                    string Title = reader["Title"].ToString();
+
+                    string Description = reader["Description"].ToString();
+
+                    string Genre = reader["Genre"].ToString();
+
+                    int Duration = (int)reader["Duration"];
+
+                    DateTime ReleaseDate =(DateTime)reader["Duration"];
+
+
+                    
+                }
+            }
+        }
+    }
+}
